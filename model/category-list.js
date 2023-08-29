773714a1a3bd49f4be2a3ebea169c23ead2dbb73
Joe <joechou2002@yahoo.com.tw>
2023-08-29 23:52:06 +0800

    initial commit

---
+++ model/category-list.js
@@ -0,0 +1,14 @@
+const categoryList = [
+    { id: "1", title: "流行樂" },
+    { id: "2", title: "古典樂" },
+    { id: "3", title: "爵士" },
+    { id: "4", title: "民歌" },
+    { id: "5", title: "電音" },
+    { id: "6", title: "嘻哈樂" },
+    { id: "7", title: "獨立音樂" },
+    { id: "8", title: "搖滾" },
+    { id: "9", title: "其他" },
+];
+// 把categoryList輸出
+module.exports = categoryList;
+// require("categoryList") => categoryList
